df430afe02a57e06ede993b922822e856216ea95
lgulich <lgulich@ethz.ch>
2021-01-14 00:35:17 +0100

    Add LICENCE

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-from pathlib import Path
-
 import setuptools
-
-file_path = Path(__file__)
-with open(file_path.parent / 'README.md', 'r') as fh:
-    LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
     name='caktin',
@@ -14,8 +8,6 @@ setuptools.setup(
     url='https://github.com/lgulich/caktin',
     download_url="https://github.com/lgulich/caktin/archive/v0.1.0.tar.gz",
     description='A tool for compiling cakti.',
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
     install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3 :: Only',
